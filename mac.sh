ec07b75af2b8bbf09a0fb33690ad0c7835dc3e14
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-13 19:31:20 +0200

    feat: add git config

---
+++ mac.sh
@@ -0,0 +1,31 @@
+brew install \
+  git \
+  fig \
+  node \
+  nvm
+
+brew install --cask \
+  webstorm \
+  visual-studio-code \
+  google-chrome \
+  docker \
+  postman \
+  slack \
+  firefox \
+  spark \
+  spotify \
+  raycast \
+  notion \
+  alt-tab \
+  numi \
+  rectangle \
+  tableplus \
+  filezilla
+
+#terminal
+git clone --recursive https://github.com/changs/slimzsh.git ~/.slimzsh
+source "$HOME/.slimzsh/slim.zsh"
+source <(curl -Ls fig.io/install)
+
+# save screenshots as jpeg (default png)
+defaults write com.apple.screencapture type jpg; killall SystemUIServer
